25b1106ffb9a4f6327709aa578a1563db65699a8
plkoko123 <jinlin9192@hotmail.com>
2018-09-13 08:07:16 -0400

    Added material.module

---
+++ src/app/material.module.ts
@@ -0,0 +1,11 @@
+
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {MatButtonModule, MatToolbarModule, MatIconModule } from '@angular/material';
+@NgModule({
+  imports: [
+    MatButtonModule, MatToolbarModule, MatIconModule
+  ],
+  declarations: []
+})
+export class MaterialModule { }
