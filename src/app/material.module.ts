e648f559dc5e78b7b911eb862f7564f51cf4e7b2
plkoko123 <jinlin9192@hotmail.com>
2018-09-13 08:18:54 -0400

    Configured typescriptHero.imports.organizeOnSave to true

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,11 +1,8 @@
-
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import {MatButtonModule, MatToolbarModule, MatIconModule } from '@angular/material';
+import { MatButtonModule, MatIconModule, MatToolbarModule } from '@angular/material';
+
 @NgModule({
-  imports: [
-    MatButtonModule, MatToolbarModule, MatIconModule
-  ],
-  declarations: []
+  imports: [MatButtonModule, MatToolbarModule, MatIconModule],
+  exports: [MatButtonModule, MatToolbarModule, MatIconModule],
 })
-export class MaterialModule { }
+export class MaterialModule {}
