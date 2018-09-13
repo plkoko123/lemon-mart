fad3b46a9683f2cd3eda4d8b9d97e81139f4db20
plkoko123 <jinlin9192@hotmail.com>
2018-09-13 08:23:48 -0400

    Revert "Added material.module"
    This reverts commit 25b1106ffb9a4f6327709aa578a1563db65699a8.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MaterialModule } from './/material.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, MaterialModule],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
