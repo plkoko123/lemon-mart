dc5e5a4acf16e15a9654a1f258f14db6d407d4b3
plkoko123 <jinlin9192@hotmail.com>
2018-09-14 10:16:51 -0400

    Added inventory-dashboard component

--- src/app/inventory/inventory.module.ts
+++ src/app/inventory/inventory.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from '@angular/core';
 import { MaterialModule } from '../material.module';
 import { InventoryRoutingModule } from './inventory-routing.module';
 import { InventoryComponent } from './inventory.component';
+import { InventoryDashboardComponent } from './inventory-dashboard/inventory-dashboard.component';
 
 @NgModule({
   imports: [CommonModule, InventoryRoutingModule, MaterialModule],
-  declarations: [InventoryComponent],
+  declarations: [InventoryComponent, InventoryDashboardComponent],
 })
 export class InventoryModule {}
