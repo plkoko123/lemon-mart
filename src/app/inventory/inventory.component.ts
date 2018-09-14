f56f593051307fdd6dd211141f1072835ceace12
plkoko123 <jinlin9192@hotmail.com>
2018-09-14 10:16:17 -0400

    Added inventory component

---
+++ src/app/inventory/inventory.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-inventory',
+  template: `
+    <p>
+      inventory works!
+    </p>
+  `,
+  styles: []
+})
+export class InventoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/inventory/inventory.module.ts
+++ src/app/inventory/inventory.module.ts
@@ -1,10 +1,12 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
+import { MaterialModule } from '../material.module';
 import { InventoryRoutingModule } from './inventory-routing.module';
+import { InventoryComponent } from './inventory.component';
 
 @NgModule({
-  imports: [CommonModule, InventoryRoutingModule],
-  declarations: [],
+  imports: [CommonModule, InventoryRoutingModule, MaterialModule],
+  declarations: [InventoryComponent],
 })
 export class InventoryModule {}
