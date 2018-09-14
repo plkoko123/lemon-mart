bbbaf489721e0c27ff67c83149b73fd8d872178c
plkoko123 <jinlin9192@hotmail.com>
2018-09-14 08:29:18 -0400

    Added user-management & receipt-lookup components

--- src/app/manager/manager.component.ts
+++ src/app/manager/manager.component.ts
@@ -3,12 +3,26 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-manager',
   template: `
-    <p>
-      manager works!
-    </p>
+    <mat-toolbar color="accent">
+      <a mat-button routerLink="/manager/home" routerLinkActive="active-link">Manager's Dashboard</a>
+      <a mat-button routerLink="/manager/users" routerLinkActive="active-link">User Management</a>
+      <a mat-button routerLink="/manager/receipts" routerLinkAcitve="active-link"> Receipt Lookup</a>
+    </mat-toolbar>
     <router-outlet></router-outlet>
   `,
-  styles: [],
+  styles: [
+    `
+      div[fxLayout] {
+        margin-top: 32px;
+      }
+    `,
+    `
+      .active-link {
+        font-weight: bold;
+        border-bottom: 2px solid #005005;
+      }
+    `,
+  ],
 })
 export class ManagerComponent implements OnInit {
   constructor() {}
