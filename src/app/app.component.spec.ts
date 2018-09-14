2fdff4448e1a0a79e11180999da0b62f794d0e95
plkoko123 <jinlin9192@hotmail.com>
2018-09-14 11:23:34 -0400

    Fixed unit tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,14 @@
-import { TestBed, async } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { async, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+
 import { AppComponent } from './app.component';
+import { MaterialModule } from './material.module';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
+      imports: [RouterTestingModule, MaterialModule, HttpClientModule],
       declarations: [AppComponent],
     }).compileComponents();
   }));
@@ -22,6 +26,8 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to lemon-mart!');
+    expect(compiled.querySelector('mat-toolbar .mat-h2').textContent).toContain(
+      'LemonMart'
+    );
   }));
 });
--- src/app/inventory/inventory.component.spec.ts
+++ src/app/inventory/inventory.component.spec.ts
@@ -1,5 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
+import { MaterialModule } from '../material.module';
 import { InventoryComponent } from './inventory.component';
 
 describe('InventoryComponent', () => {
@@ -9,6 +11,7 @@ describe('InventoryComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [InventoryComponent],
+      imports: [MaterialModule, RouterTestingModule],
     }).compileComponents();
   }));
 
