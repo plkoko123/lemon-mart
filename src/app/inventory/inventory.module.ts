7bed99758f466a2c82955c397d4c43719768a9b1
plkoko123 <jinlin9192@hotmail.com>
2018-09-14 10:18:48 -0400

    Added categories component

--- src/app/inventory/inventory.module.ts
+++ src/app/inventory/inventory.module.ts
@@ -7,9 +7,10 @@ import { InventoryComponent } from './inventory.component';
 import { InventoryDashboardComponent } from './inventory-dashboard/inventory-dashboard.component';
 import { StockEntryComponent } from './stock-entry/stock-entry.component';
 import { ProductsComponent } from './products/products.component';
+import { CategoriesComponent } from './categories/categories.component';
 
 @NgModule({
   imports: [CommonModule, InventoryRoutingModule, MaterialModule],
-  declarations: [InventoryComponent, InventoryDashboardComponent, StockEntryComponent, ProductsComponent],
+  declarations: [InventoryComponent, InventoryDashboardComponent, StockEntryComponent, ProductsComponent, CategoriesComponent],
 })
 export class InventoryModule {}
