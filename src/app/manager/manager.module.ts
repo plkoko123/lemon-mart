21912e33e10f98771e48b5c5bff3a3e7cb3976bf
plkoko123 <jinlin9192@hotmail.com>
2018-09-14 06:29:12 -0400

    Added manager-component

--- src/app/manager/manager.module.ts
+++ src/app/manager/manager.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { ManagerRoutingModule } from './manager-routing.module';
 import { ManagerHomeComponent } from './manager-home/manager-home.component';
+import { ManagerComponent } from './manager.component';
 
 @NgModule({
   imports: [CommonModule, ManagerRoutingModule],
-  declarations: [ManagerHomeComponent],
+  declarations: [ManagerHomeComponent, ManagerComponent],
 })
 export class ManagerModule {}
