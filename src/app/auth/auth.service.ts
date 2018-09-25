4db7465d0a50f375282c069be0f1f0744715ab3e
plkoko123 <jinlin9192@hotmail.com>
2018-09-24 23:56:34 -0400

    Coded auth.service

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,9 +1,69 @@
 import { Injectable } from '@angular/core';
+import { sign } from 'fake-jwt-sign';
+import {
+  BehaviorSubject,
+  Observable,
+  of,
+  throwError as observableThrowError,
+} from 'rxjs';
+import { HttpClient } from 'selenium-webdriver/http';
+
+import { Role } from './role.enum';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  private readonly authProvider: (
+    email: string,
+    password: string
+  ) => Observable<IServerAuthResponse>;
 
-  constructor() { }
+  authStatus = new BehaviorSubject<IAuthStatus>(defaultAuthStatus);
+
+  constructor(private httpClient: HttpClient) {
+    // Fake login function to simulate roles
+    this.authProvider = this.fakeAuthProvider;
+  }
+
+  private fakeAuthProvider(
+    email: string,
+    password: string
+  ): Observable<IServerAuthResponse> {
+    if (!email.toLowerCase().endsWith('@test.com')) {
+      return observableThrowError('Failed to login! Email needs to end with @test.com');
+    }
+    const authStatus = {
+      isAuthenticated: true,
+      userId: 'e4d1bc2ab25c',
+      userRole: email.toLowerCase().includes('cashier')
+        ? Role.Cashier
+        : email.toLowerCase().includes('clerk')
+          ? Role.Clerk
+          : email.toLowerCase().includes('manager')
+            ? Role.Manager
+            : Role.None,
+    } as IAuthStatus;
+
+    const authResponse = {
+      accessToken: sign(authStatus, 'secret', {
+        expiredIn: '1h',
+        algorithm: 'none',
+      }),
+    } as IServerAuthResponse;
+
+    return of(authResponse);
+  }
 }
+
+export interface IAuthStatus {
+  isAuthenticated: boolean;
+  userRole: Role;
+  userId: string;
+}
+
+interface IServerAuthResponse {
+  accessToken: string;
+}
+
+const defaultAuthStatus = { isAuthenticated: false, userRole: Role.None, userId: null };
