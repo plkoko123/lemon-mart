f9bd5e7f57ee4515ef19191549bfd3f4f6fdf5a5
plkoko123 <jinlin9192@hotmail.com>
2018-09-27 11:55:15 -0400

    Added view-user.component

---
+++ src/app/user/view-user/view-user.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-user',
+  template: `
+    <p>
+      view-user works!
+    </p>
+  `,
+  styles: []
+})
+export class ViewUserComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
