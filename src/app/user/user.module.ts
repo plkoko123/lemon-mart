de4d29dffb2cae0c661cdcbb5ab9cb3975dd30d8
plkoko123 <jinlin9192@hotmail.com>
2018-09-27 11:38:11 -0400

    Imported required modules to user.module

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,14 +1,30 @@
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { MaterialModule } from '../app-material.module';
+import { AuthGuard } from '../auth/auth-guard.service';
 import { LogoutComponent } from './logout/logout.component';
 import { NavigationMenuComponent } from './navigation-menu/navigation-menu.component';
 import { ProfileComponent } from './profile/profile.component';
+import { UserMaterialModule } from './user-material.module';
 import { UserRoutingModule } from './user-routing.module';
+import { UserService } from './user/user.service';
 
 @NgModule({
-  imports: [CommonModule, UserRoutingModule, MaterialModule],
+  imports: [
+    CommonModule,
+    HttpClientModule,
+    UserRoutingModule,
+    MaterialModule,
+    UserMaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
+    FlexLayoutModule,
+  ],
   declarations: [ProfileComponent, LogoutComponent, NavigationMenuComponent],
+  providers: [UserService, AuthGuard],
 })
 export class UserModule {}
