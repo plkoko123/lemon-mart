08a72a9eb50d0aae3f64fe79b0d8ce978e871b7a
plkoko123 <jinlin9192@hotmail.com>
2018-09-27 11:19:39 -0400

    Coded profile.component model form

--- src/app/user/profile/profile.component.ts
+++ src/app/user/profile/profile.component.ts
@@ -1,4 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { $enum } from 'ts-enum-util';
+
+import { AuthService } from '../../auth/auth.service';
+import { Role as UserRole } from '../../auth/role.enum';
+import {
+  BirthDateValidation,
+  EmailValidation,
+  OneCharValidation,
+  OptionalTextValidation,
+  RequiredTextValidation,
+  USAZipCodeValidation,
+} from '../../common/validation';
+import { IUser } from '../user/user';
+import { UserService } from '../user/user.service';
+import { IUSState, PhoneType } from './data';
 
 @Component({
   selector: 'app-profile',
@@ -6,7 +24,66 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css'],
 })
 export class ProfileComponent implements OnInit {
-  constructor() {}
+  Role = UserRole;
+  PhoneTypes = $enum(PhoneType).getKeys();
+  userForm: FormGroup;
+  states: Observable<IUSState[]>;
+  userError = '';
+  currentUserRole = this.Role.None;
 
-  ngOnInit() {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private userService: UserService,
+    private authService: AuthService
+  ) {}
+
+  ngOnInit() {
+    this.authService.authStatus.subscribe(
+      authStatus => (this.currentUserRole = authStatus.userRole)
+    );
+    this.userService.getCurrentUser().subscribe(user => this.buildUserForm(user));
+    this.buildUserForm();
+  }
+
+  buildUserForm(user?: IUser) {
+    this.userForm = this.formBuilder.group({
+      email: [
+        {
+          value: (user && user.email) || '',
+          disabled: this.currentUserRole !== this.Role.Manager,
+        },
+        EmailValidation,
+      ],
+      name: this.formBuilder.group({
+        first: [(user && user.name.first) || '', RequiredTextValidation],
+        middle: [(user && user.name.middle) || '', OneCharValidation],
+        last: [(user && user.name.last) || '', RequiredTextValidation],
+      }),
+      role: [
+        {
+          value: (user && user.role) || '',
+          disabled: this.currentUserRole !== this.Role.Manager,
+        },
+        [Validators.required],
+      ],
+      dateOfBirth: [(user && user.dateOfBirth) || '', BirthDateValidation],
+      address: this.formBuilder.group({
+        line1: [
+          (user && user.address && user.address.line1) || '',
+          RequiredTextValidation,
+        ],
+        line2: [
+          (user && user.address && user.address.line2) || '',
+          OptionalTextValidation,
+        ],
+        city: [(user && user.address && user.address.city) || '', RequiredTextValidation],
+        state: [
+          (user && user.address && user.address.state) || '',
+          RequiredTextValidation,
+        ],
+        zip: [(user && user.address && user.address.zip) || '', USAZipCodeValidation],
+      }),
+    });
+  }
 }
