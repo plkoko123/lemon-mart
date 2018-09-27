9d83f6f1a55d7bfb9e2424b04288f32484f3b797
plkoko123 <jinlin9192@hotmail.com>
2018-09-27 12:34:43 -0400

    Added view-user.component

--- src/app/user/view-user/view-user.component.ts
+++ src/app/user/view-user/view-user.component.ts
@@ -1,19 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { IUser, User } from '../user/user';
 
 @Component({
   selector: 'app-view-user',
   template: `
-    <p>
-      view-user works!
-    </p>
+    <mat-card>
+      <mat-card-header>
+        <div mat-card-avatar><mat-icon>account_circle</mat-icon></div>
+        <mat-card-title>{{currentUser.fullName}}</mat-card-title>
+        <mat-card-subtitle>{{currentUser.role}}</mat-card-subtitle>
+      </mat-card-header>
+      <mat-card-content>
+        <p><span class="mat-input bold">E-mail</span></p>
+        <p>{{currentUser.email}}</p>
+        <p><span class="mat-input bold">Date of Birth</span></p>
+        <p>{{currentUser.dateOfBirth | date:'mediumDate'}}</p>
+      </mat-card-content>
+      <mat-card-actions *ngIf="editMode">
+        <button mat-button mat-raised-button>Edit</button>
+      </mat-card-actions>
+    </mat-card>
   `,
-  styles: []
+  styles: [
+    `
+      .bold {
+        font-weight: bold;
+      }
+    `,
+  ],
 })
 export class ViewUserComponent implements OnInit {
+  @Input()
+  user: IUser;
+  currentUser = new User();
 
-  constructor() { }
-
-  ngOnInit() {
+  get editMode() {
+    return !this.user;
   }
 
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    if (this.user) {
+      this.currentUser = User.BuildUser(this.user);
+    }
+
+    if (this.route.snapshot && this.route.snapshot.data) {
+      this.currentUser = User.BuildUser(this.route.snapshot.data['user']);
+      this.currentUser.dateOfBirth = Date.now(); // for data mocking purposes only
+    }
+  }
 }
