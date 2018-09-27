7cef1eeff3b7d773a702d7997ab3f4b5a346e36a
plkoko123 <jinlin9192@hotmail.com>
2018-09-27 08:20:00 -0400

    Added user.service

---
+++ src/app/user/user/user.service.ts
@@ -0,0 +1,56 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+import { environment } from '../../../environments/environment';
+import { AuthService, IAuthStatus } from '../../auth/auth.service';
+import { CacheService } from '../../auth/cache.service';
+import { transformError } from '../../common/common';
+import { IUser, User } from './user';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService extends CacheService {
+  currentUser = new BehaviorSubject<IUser>(this.getItem('user') || new User());
+  private currentAuthStatus: IAuthStatus;
+  constructor(private httpClient: HttpClient, private authService: AuthService) {
+    super();
+    this.currentUser.subscribe(user => this.setItem('user', user));
+    this.authService.authStatus.subscribe(
+      authStatus => (this.currentAuthStatus = authStatus)
+    );
+  }
+
+  getCurrentUser(): Observable<IUser> {
+    const userObservable = this.getUser(this.currentAuthStatus.userId).pipe(
+      catchError(transformError)
+    );
+    userObservable.subscribe(
+      user => this.currentUser.next(user),
+      err => Observable.throw(err)
+    );
+    return userObservable;
+  }
+
+  getUser(id): Observable<IUser> {
+    return this.httpClient.get<IUser>(`${environment.baseUrl}/v1/user/${id}`);
+  }
+
+  updateUser(user: IUser): Observable<IUser> {
+    this.setItem('draft-user', user); // cache user data in case of errors
+    const updateResponse = this.httpClient
+      .put<IUser>(`${environment.baseUrl}/v1/user/${user.id || 0}`, user)
+      .pipe(catchError(transformError));
+
+    updateResponse.subscribe(
+      res => {
+        this.currentUser.next(res);
+        this.removeItem('draft-user');
+      },
+      err => Observable.throw(err)
+    );
+    return updateResponse;
+  }
+}
