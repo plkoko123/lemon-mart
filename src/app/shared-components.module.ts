aeb0c7c6afea747a9cf19af351d87983f069836c
plkoko123 <jinlin9192@hotmail.com>
2018-10-02 09:55:55 -0400

    Added shred-components.module.ts

---
+++ src/app/shared-components.module.ts
@@ -0,0 +1,21 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { MaterialModule } from './app-material.module';
+import { ViewUserComponent } from './user/view-user/view-user.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    FlexLayoutModule,
+    FlexLayoutModule,
+    MaterialModule,
+  ],
+  declarations: [ViewUserComponent],
+  exports: [ViewUserComponent],
+})
+export class ShareComponentsModule {}
